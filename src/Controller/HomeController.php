93016dd348f015b5f80a54ba1a0a877fe65fcd50
DylanG <dev.dyger@gmail.com>
2021-11-10 08:31:00 +0100

    Mise en place de Encore

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,44 +1,26 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController 
 {
-
-    /**
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello", name="hello_base")
-     * @Route("/hello/{prenom}/", name="hello_prenom")
-     * Montre la page qui dit Bonjour
-     * @return void
-     */
-    public function hello($prenom ="anonyme", $age =0)
-    {
-        return $this->render(
-            'hello.html.twig', 
-            [
-                'prenom' => $prenom,
-                'age'    => $age
-            ]
-            );
-    }
     /**
      * @Route("/", name="homepage")
      *
      * @return void
      */
-    public function home()
+    public function home(AdRepository $adRepository, UserRepository $userRepository)
     {
-        $prenoms = ["Lior" => 31, "Joseph" => 12, "Anne" => 55];
        return $this->render(
            'home.html.twig',
-           [
-               'title'   => "Bonjour à tous",
-               'age'     => 15,
-               'tableau' => $prenoms               
+            [
+                'ads' =>$adRepository->findBestAds(3),
+                'users' =>$userRepository->findBestUsers(2)
             ]
        );
     }
